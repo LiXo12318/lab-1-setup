b30dce2a2b51d2f81e5c4c49f77ace9cf680bc29
Maksym Koshyn <maksym.koshyn@oa.edu.ua>
2026-04-05 14:49:05 +0300

    Fix: add base path for GitHub Pages

--- web/vitest.config.ts
+++ web/vitest.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
+  base: '/lab-1-setup/',
   plugins: [react(), tsconfigPaths()],
   test: {
     globals: true,
